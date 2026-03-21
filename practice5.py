488dd747fc38577006e9f35bf71ea030968bc5ce
Umang <umang.mandilwar@lilly.com>
2026-03-21 22:32:35 +0530

    Adding the list practice file

---
+++ practice5.py
@@ -0,0 +1,35 @@
+# Lists
+
+mylist=[]
+print(mylist)
+print(type(mylist))
+
+# other way to create an empty list
+mylist1=list()
+print(mylist1)
+
+games=["Cricket", "Football", "Hockey", "Tennis"]
+print(games)
+print(games[0])
+print(games[2])
+print(games[-1])
+print(games[::-1])
+print(games[1:3])
+print(games[-1] == games[3])
+games[2] = "Badminton"
+print(games)
+games.append("Basketball")
+print(games)
+
+l1=[10,20,30,40,50]
+l2=[1.5,2.4,3.6,4.8,5.6]
+l3=[True, False, False, True]
+l4=["Umang", "Sanjeev", 25, 54, True]
+print(l1)
+print(l2)
+print(l3)
+print(l4)
+l1[0]+=100
+print(l1)
+l1[3]*=3
+print(l1)
